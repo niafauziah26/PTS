5b1e995c2de5548362536c8f31a6db64863b98df
niafauziah1606@gmail.com <niafauziah1606@gmail.com>
2024-04-28 19:39:58 +0700

    mengubah

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ import {
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAbLsq27KJU9tD9pHC8GrPUB7LgEPEQbPU",
-    authDomain: "insan-cemerlang-92ee0.firebaseapp.com",
-    projectId: "insan-cemerlang-92ee0",
-    storageBucket: "insan-cemerlang-92ee0.appspot.com",
-    messagingSenderId: "332441427242",
-    appId: "1:332441427242:web:73c31309147ef1dab15253",
-    measurementId: "G-JW04DZL85R"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyD_N_5Tjv9zh_eJyfTXxUOICM2XX86--IM",
+  authDomain: "datasiswa-aebb3.firebaseapp.com",
+  projectId: "datasiswa-aebb3",
+  storageBucket: "datasiswa-aebb3.appspot.com",
+  messagingSenderId: "1049128187878",
+  appId: "1:1049128187878:web:e1879710f4b5252a68c827",
+};
+
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
